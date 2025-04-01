6ae4b04b56732e9fb1e9091a4224e231208b16f7
VIJAYANAARAAYANAN <vijaybalaram05@gmail.com>
2025-04-01 17:50:43 +0530

    Starting SignUp login page

---
+++ src/Pages/SignUpPage.tsx
@@ -0,0 +1,10 @@
+import { Divider } from "@mantine/core";
+
+const SignUpPage = () => {
+  return (
+    <div className="min-h-[100vh]  bg-[#eaf1f5] font-[Poppins]">
+      <Divider mr="xs" size="xs" mx="md" />
+    </div>
+  );
+};
+export default SignUpPage;
